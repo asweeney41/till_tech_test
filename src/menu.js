9e86992911e6f3a85e647406126b715b877e0df4
user <user@users-MacBook-Air-2.local>
2016-01-14 12:06:07 +0000

    setup

---
+++ src/menu.js
@@ -0,0 +1,9 @@
+function Menu(){
+
+var request = new XMLHttpRequest();
+request.open("GET", "../../data/file.json", false);
+request.send(null);
+var my_JSON_object = JSON.parse(request.responseText);
+alert (my_JSON_object.result[0]);
+
+}
